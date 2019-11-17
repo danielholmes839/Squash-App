7fedb84ae532033525f40947eddc80f7f2be0c8d
Daniel <danielh839@gmail.com>
2019-11-17 13:06:01 -0500

    Added Config Objects

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_heroku import Heroku
 
+
+class DevelopmentConfig(object):
+    def __init__(self):
+        self.DEBUG = True
+        self.SQLAlCHEMY_DATABASE_URI = 'sqlite://test.db'
+        self.SECRET_KEY = 'secret'
+
+
+class ProductionConfig(object):
+    def __init__(self):
+        self.DEBUG = False
+        self.SQLAlCHEMY_DATABASE_URI = os.environ['DB_URI']
+        self.SECRET_KEY = os.environ['SECRET_KEY']
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['ELEPHANT_POSTGRESQL_DB']
+app.config.from_object(ProductionConfig())
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
